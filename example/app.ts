7dd580267815243869f355914f74476197b8bf5f
eldeni <eldeniyenden@gmail.com>
2018-11-18 21:41:32 +0900

    feat: define `createRouter()`, replacing `routeMapper()`
    - routeMapper() created and also registered express.Router. This made it uneasy
    to attach middleware into the express application in the uniform manner. Now
    that createRouter() is defined and the method only returns Router, caller of
    createRouter() can attach Router as middleware at his disposal.
    - example is also added. `@babel/register` is used to transpile the example at
    runtime.
    
    Fixes https://github.com/nodekit-org/route-mapper/issues/1

---
+++ example/app.ts
@@ -0,0 +1,65 @@
+import express, {
+  Request,
+  Response,
+  NextFunction,
+} from 'express';
+
+import { 
+  createRouter,
+  Route,
+} from '../lib/index';
+
+const PORT = 5100;
+
+const app = express();
+
+const routes0: Route<string>[] = [
+  {
+    action: () => {
+      return Promise.resolve('debug');
+    },
+    method: 'get',
+    path: '/debug',
+  },
+];
+
+const routes1: Route<string>[] = [
+  {
+      action: () => Promise.resolve('seed'),
+      method: 'get',
+      path: '/seed',
+    },
+    {
+      action: () => Promise.resolve('comment'),
+      method: 'post',
+      path: '/comment',
+    },
+];
+
+function respond(req: Request, res: Response, next: NextFunction) {
+  return function (data) {
+    res.status(200)
+      .send({
+        data,
+      });
+    return data;
+  };
+}
+
+const v0 = createRouter(routes0, [ respond ]);
+const v1 = createRouter(routes1, [ respond ]);
+
+app.use((req, res, next) => {
+  console.info(req.url, new Date());
+  next();
+});
+
+app.use('/v0', v0);
+app.use('/v1', v1);
+
+app.listen(PORT, function(err) {
+  if (err) {
+    console.error(err);
+  }
+  console.info('Listening at port: %s', PORT);
+});
