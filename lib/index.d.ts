e04307139fed6a2b750ba52c02038e3b2e09f231
eldeni <eldeniyenden@gmail.com>
2018-11-25 20:11:29 +0900

    refactor: build to remove console logging

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,19 +1,11 @@
-import { Application, NextFunction, Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 export declare const createRouter: CreateRouter;
-export interface Route<R> {
+interface Route<R> {
     action: (x: object | null) => Promise<R>;
     beforeware?: Array<(Request: any, res: Response, next: NextFunction) => void>;
     createParam?: (req: Request) => object;
     method: 'get' | 'post' | 'put' | 'delete';
     path: string;
-}
-export interface RegisterRouter {
-    <R>(app: Application, mapPath: string, routeMap: Route<R>[], postAsyncHandlers: Array<PostAsyncHandler<R>>): void;
-}
-export interface RouteDefinition<R> {
-    map: Route<R>[];
-    mapPath: string;
-    postAsyncHandlers: Array<(req: Request, res: Response, next: NextFunction) => (R: R) => R>;
 }
 interface PostAsyncHandler<R> {
     (req: Request, res: Response, next: NextFunction): (R: R) => R;
