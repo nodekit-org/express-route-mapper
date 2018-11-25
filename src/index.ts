647aef20a297867d0761d247bbcfb200204011a4
eldeni <eldeniyenden@gmail.com>
2018-11-25 20:55:45 +0900

    feat: modify color of a log tag
    - hex `D1D2FD` is used.

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,18 @@ import {
 } from 'express';
 import chalk from 'chalk';
 
+const tag = chalk.hex('D1D2FD')('[routeMapper]');
+
 export const createRouter: CreateRouter = function (routes, postAsyncHandlers) {
   const router: Router = Router();
-  console.log(`[routeMapper] postAsyncHandlers (%s), routes (%s)`, postAsyncHandlers.length, routes.length);
+  console.log(
+    `${tag} Following routes (%s) will be registered with postAsyncHandlers (%s)`,
+    routes.length, 
+    postAsyncHandlers.length
+  );
 
   routes.map((route) => {
-    console.debug(`[routeMapper] Route is registered: [%s] ${chalk.green('%s')}`, route.method, route.path);
+    console.debug(`${tag} Route is registered: [%s] ${chalk.green('%s')}`, route.method, route.path);
     router[route.method](
       route.path, 
       [
